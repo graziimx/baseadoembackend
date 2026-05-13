db6ad324b86d38dc35cb47fdd66e8afeaa7517be
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-05-13 12:40:12 +0000

    versao3

---
+++ desafio.js
@@ -0,0 +1,19 @@
+// Cadastro de Aluno
+
+const nomeCompleto = "João Silva Sauro";
+const numeroMatricula = 2024001
+const emailContato = "joao.silva@escola.com"
+const dataNascimento = "2010-05-15"
+const statusMatricula = "Ativa"
+
+let notaMatematica = 8
+let notaPortuguês = 6
+
+let somaDasnotas = notaMatematica + notaPortuguês
+let mediaFinal = somaDasnotas / 2
+
+console.log (`a nota dele em matematica e ${notaMatematica} e a nota em português e ${notaPortuguês} e a media dessas notas é ${mediaFinal}`)
+
+console.log ( `
+    o nome completo dele é ${nomeCompleto}, ele nasceu em ${dataNascimento}, o email de contato é ${emailContato},
+     o número de matrícula é ${numeroMatricula} e a matrícula dele ainda tá ${statusMatricula}`)
