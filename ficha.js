8c9443a2d5f9e3793b0ed0a7af9885217bc3f5df
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-06-01 11:51:49 +0000

    versao1

---
+++ ficha.js
@@ -0,0 +1,11 @@
+//Criar um objeto que represente o mundo real
+const animal = {
+    nome:"Thor",
+    especie:"Cachorro",
+idade:4,
+tutor:true,
+vacinado: false 
+};
+
+console.log(animal.nome);
+console.log(animal.tutor);
