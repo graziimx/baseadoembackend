0b6d5cb5df476df2631a2287a78ad4f6e5e4d0dc
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-06-22 12:13:15 +0000

    versão1

---
+++ escola.js
@@ -0,0 +1,14 @@
+const dadosDoAluno = require('./aluno.json');
+
+console.log("--- Sistema escolar ---");
+console.log(`Nome: ${dadosDoAluno.nome}`);
+console.log(`Curso: ${dadosDoAluno.curso}`);
+console.log (`Idade : ${dadosDoAluno.idade}`);
+if (dadosDoAluno.bolsista) {
+    console.log ("Status: Este aluno possui isenção de mensalidade.");
+} else{
+  console.log("Status: Mensalidade padrão");
+};
+
+//Alterar o tipo de bolsa
+//Adicione a saida idade
