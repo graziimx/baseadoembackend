97330db4ebc80ef633931e714f1ed01db0e3ba74
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-08-18 10:51:35 +0000

    "salva1"

--- time.js
+++ time.js
@@ -1,33 +1,38 @@
 class Time {
-constructor(jogador, tecnica, cartoes, gol) {
-this.jogador = jogador;
-this.tecnica = tecnica;
-this.cartoes = cartoes;
-this.gol = gol;
+    constructor(jogador, tecnica, cartoes, gol) {
+        this.jogador = jogador;
+        this.tecnica = tecnica;
+        this.cartoes = cartoes;
+        this.gol = gol;
+    }
+
+    apresentar() {
+        console.log(`O ${this.jogador} fez ${this.gol} gols.`);
+    }
 }
 
-apresentar() {
-console.log(`O ${this.jogador} fez
-${this.gol}`);
-}
+class TimeFutebol extends Time {
+    constructor(jogador, tecnica, cartoes, gol, nomeTime, tecnico) {
+        super(jogador, tecnica, cartoes, gol);
+        this.nomeTime = nomeTime;
+        this.tecnico = tecnico;
+    }
+
+    mostrarTime() {
+        console.log("Time:", this.nomeTime);
+        console.log("Técnico:", this.tecnico);
+        console.log("Técnica:", this.tecnica);
+        console.log("Cartões:", this.cartoes);
+    }
 }
 
-class timefutebol extends Time {
-constructor(jogador, tecnicas, cartoes, gol) {
-super(jogador, tecnicas, cartoes, gol);
-}
-}
-
-mostrarTime() {
-console.log("time:",meutime Santos `${this.nomeTime}`);
-console.log("tecnica:",meutecnico Renato Gaucho `${this.tecnicas}`);
-}
-let time = new timeFutebol(
-“Neymar”,
-“Renato Gaucho”,
-2,
-3,
-“Santos”,
+let time = new TimeFutebol(
+    "Neymar",
+    "Drible",
+    2,
+    3,
+    "Santos",
+    "Renato Gaúcho"
 );
 
 time.apresentar();
